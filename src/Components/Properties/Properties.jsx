e75f2cdd50a9253b0216863c355bbb106ad28812
Negsvillian12 <oluwatosinoluwatoba@gmail.com>
2024-03-23 15:20:50 +0100

    added

--- src/Components/Properties/Properties.jsx
+++ src/Components/Properties/Properties.jsx
@@ -8,10 +8,11 @@ import Apartment from "./cards/apartment";
 import Outlets from "./cards/outlet";
 import Office from "./cards/offices";
 import Practice from "./cards/practice";
-import Real from "./cards/real";
+import Practice2 from "./cards/Practice2";
+// import Real from "./cards/real";
 
 import realEstateAbi from '../../contracts/realEstateAbi.json';
-import NFTViewer from "./cards/viewer";
+// import NFTViewer from "./cards/viewer";
 
 function PropertyPage() {
   const [web3, setWeb3] = useState(null);
@@ -156,8 +157,9 @@ function PropertyPage() {
       <Office />
       <Outlets />
       <Practice />
-      <Real />
-      <NFTViewer />
+      <Practice2 />
+      {/* <Real />  */}
+      {/* <NFTViewer /> */}
     </div>
 
   );
